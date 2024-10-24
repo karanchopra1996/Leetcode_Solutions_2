e46337ad65c9b324af19efa07d2f3a88fbed4a09
Karan Chopra <114980007+karanchopra1996@users.noreply.github.com>
2024-10-23 19:18:27 -0700

    Time: 4 ms (87.69%), Space: 45.4 MB (46.05%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>(numCourses);
+        for(int i = 0; i < numCourses; i++){
+            adj.add(new ArrayList<>());
+        }
+        
+        for(int[] prerequisite : prerequisites){
+            adj.get(prerequisite[1]).add(prerequisite[0]);
+        }
+        
+        boolean[] visit = new boolean[numCourses];
+        boolean[] inStack = new boolean[numCourses];
+        for(int i =0; i < numCourses; i++){
+            if(dfs(i,adj,visit,inStack)){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public boolean dfs(int node, List<List<Integer>> adj, boolean[] visit, boolean[] inStack){
+       // if the node is already in the stack, we have a cycle
+        if(inStack[node]){
+            return true;
+        }
+        if(visit[node]){
+            return false;
+        }
+        //Mark the current node as visited and part of current recursion
+        
+        visit[node] = true;
+        inStack[node] = true;
+        
+        for(int neighbor: adj.get(node)){
+            if(dfs(neighbor, adj, visit, inStack)){
+                return true;
+            }
+        }
+        
+        // remove the node from the stack
+        inStack[node] = false;
+        return false;
+        
+    }
+}
