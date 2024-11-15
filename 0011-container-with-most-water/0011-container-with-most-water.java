89ab0abb0e82e6e8b372e15ba9b0f826dd3bcb49
Karan Chopra <114980007+karanchopra1996@users.noreply.github.com>
2024-11-15 12:59:33 -0800

    Time: 5 ms (74.10%), Space: 57.8 MB (53.64%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -6,13 +6,13 @@ public class Solution {
         
         // Loop until the two pointers meet
         while (left < right) {
-            // Calculate the width 
-            int width = right - left;
+            // Calculate the length 
+            int length = right - left;
             
             // Calculate the area between the two lines & update maxArea
             maxArea = Math.max(
                 maxArea,
-                Math.min(height[left], height[right]) * width
+                Math.min(height[left], height[right]) * length
             );
             // Move the pointer that's at the shorter heightand trying to find a taller one
             if (height[left] <= height[right]) {
