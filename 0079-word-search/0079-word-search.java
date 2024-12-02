da342572e5260308af8df29efdd4c382e2d82b13
Karan Chopra <114980007+karanchopra1996@users.noreply.github.com>
2024-12-01 16:23:20 -0800

    Time: 135 ms (77.98%), Space: 41.9 MB (28.58%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -17,21 +17,21 @@ class Solution {
     
     // DFS function to search the word in the grid
     public boolean dfs(char[][] board, String word, int i, int j, int index) {
-        // If we have matched all letters in the word, return true
+        
         if (index == word.length()) {
             return true;
         }
         
-        // Check the boundaries of the grid and if the character doesn't match
+        
         if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(index)) {
             return false;
         }
         
-        // Temporarily mark the cell as visited by replacing the character
+        
         char temp = board[i][j];
         board[i][j] = '#';  // Mark as visited
         
-        // Explore all four directions: up, down, left, right
+        // Explore all four directionsup
         boolean found = dfs(board, word, i - 1, j, index + 1) || // Up
                         dfs(board, word, i + 1, j, index + 1) || // Down
                         dfs(board, word, i, j - 1, index + 1) || // Left
